31147669cf8c64a8cbbc75d28931a830353d1a71
Altynbek <rysbek20051@gmail.com>
2023-04-16 20:49:27 +0600

    add MyArrayList contains method

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,24 @@
 public class MyArrayList {
+    private Object[] elements;
+    private int size;
+
+    public  MyArrayList() {
+        elements = new Object[10];
+        size = 0;
+    }
+
+
+    public int size() {
+        return size;
+    }
+
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
